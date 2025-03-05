8360697d400346894132d29bbb32c6b835cd0561
gustavo <chimenes61@gmail.com>
2025-03-05 07:44:06 -0300

    Create app

---
+++ LittleLemon/LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Gustavo on 05/03/25.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
